93f764b68f8372cb930942eb771080a81df3d0e7
Josh Hawkins <jkhaccounts@gmail.com>
2016-09-22 17:34:41 -0500

    0.3.1 - Fixed manifest issue, reupload to PyPi

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name = 'shawk',
     packages = ['shawk'],
-    version = '0.3',
+    version = '0.3.1',
     description = 'A simple and user-friendly SMTP to SMS library',
     author = 'Josh Hawkins',
     author_email = 'jkhaccounts@gmail.com',
     url = 'https://github.com/hawkins/shawk',
-    download_url = 'https://github.com/hawkins/shawk/tarball/0.1',
+    download_url = 'https://github.com/hawkins/shawk/tarball/0.3.1',
     keywords = ['sms', 'smtp', 'imap', 'hawkins', 'simple'], # arbitrary keywords
     classifiers = [],
     install_requires=[
