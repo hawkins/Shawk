813cdcc6e030273c046e9ae069728f5098a452fa
Josh Hawkins <jkhaccounts@gmail.com>
2016-10-18 16:11:34 -0500

    0.4

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name = 'shawk',
     packages = ['shawk'],
-    version = '0.3.1',
+    version = '0.4',
     description = 'A simple and user-friendly SMTP to SMS library',
     author = 'Josh Hawkins',
     author_email = 'jkhaccounts@gmail.com',
     url = 'https://github.com/hawkins/shawk',
-    download_url = 'https://github.com/hawkins/shawk/tarball/0.3.1',
+    download_url = 'https://github.com/hawkins/shawk/tarball/0.4',
     keywords = ['sms', 'smtp', 'imap', 'hawkins', 'simple'], # arbitrary keywords
     classifiers = [],
     install_requires=[
